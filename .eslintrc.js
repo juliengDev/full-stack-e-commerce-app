ce09d3e02b79b6412e95d60133bfb778914df0de
Julien Gilbert <juliengilbert.dev@gmail.com>
2024-10-16 18:01:11 +0700

    header section done

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,4 +3,14 @@ module.exports = {
   extends: ['plugin:@next/next/recommended', '@payloadcms'],
   ignorePatterns: ['**/payload-types.ts'],
   plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'no-console': 'off',
+  },
+
+  plugins: ['simple-import-sort'],
+  rules: {
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+  },
 }
