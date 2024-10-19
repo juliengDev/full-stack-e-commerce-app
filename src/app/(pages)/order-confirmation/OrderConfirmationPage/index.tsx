db5f353aadaec4d398e08f1cd606be4a4423bad6
Julien Gilbert <juliengilbert.dev@gmail.com>
2024-10-19 15:47:19 +0700

    account section done

--- src/app/(pages)/order-confirmation/OrderConfirmationPage/index.tsx
+++ src/app/(pages)/order-confirmation/OrderConfirmationPage/index.tsx
@@ -44,7 +44,13 @@ export const OrderConfirmationPage: React.FC<{}> = () => {
             {`Your order has been confirmed. You will receive an email confirmation shortly. Your order ID is ${orderID}.`}
           </p>
           <div className={classes.actions}>
-            <Button href={`/orders/${orderID}`} label="View order" appearance="primary" />
+            {/* <Button href={`/account/orders/${orderID}`} label="View order" appearance="primary" />
+            <Button
+              href={`${process.env.NEXT_PUBLIC_SERVER_URL}/account/orders`}
+              label="View all orders"
+              appearance="secondary"
+            /> */}
+            <Button href={`/orders/`} label="View order" appearance="primary" />
             <Button
               href={`${process.env.NEXT_PUBLIC_SERVER_URL}/orders`}
               label="View all orders"
